b8729da8ba745f169794a4dbfe1992a213da5e20
Nick <nickmarron@hotmail.co.uk>
2020-05-06 19:43:05 +0100

    kata 4 completed.

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+const catYrs = (number - 2) * 4 + 24
+const dogYrs = (number - 2) * 5 + 24
+const yrOne = 15
+const yrTwo = 24
+
+
+if (number > 3){
+    return array_num = [number, catYrs, dogYrs];
+}
+else if(number === 1){
+    return array_num = [number, yrOne, yrOne];
+}
+else if (number === 2) {
+    return array_num = [number, yrTwo, yrTwo];
+} else {
+    return "Please provide a whole number not minus or decimal"
+};
+};
+
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -3,9 +3,13 @@ const { humanCatDogYears } = require("../src");
 // Look Ma, no handlebars!!!
 
 describe("humanCatDogYears", () => {
-    test("", () => {
-      expect(humanCatDogYears(3)).toBe('Fizz');
+    test("return cat, dog and human years based on human years passing through", () => {
+      
 
-  
+        
+        expect(humanCatDogYears(10)).toEqual([10,56,64]);
+        expect(humanCatDogYears(1)).toEqual([1,15,15]);
+        expect(humanCatDogYears(-1)).toEqual("Please provide a whole number not minus or decimal");
+        expect(humanCatDogYears(11.1)).toEqual([11.1, 60.4, 69.5]);
     });
 }); 
