7f6f1e72db231f4070df4dfd6ec92a2861789fe6
Nick <nickmarron@hotmail.co.uk>
2020-05-07 20:48:37 +0100

    kata 6 complete - names

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,23 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+let jointNames = '';
+let chrDef = '';
+
+for (let i=0; i<namesObj.length; i+=1) {
+
+    if (i === namesObj.length - 1) {
+        chrDef = ''
+    } 
+    else if (i === namesObj.length - 2) {
+        chrDef = ' & '
+    }
+    else {
+        chrDef = ', '
+    };
+    jointNames += namesObj[i].name + chrDef;
+
+  };
+  return jointNames;
+
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,10 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toBe('Bart, Lisa & Maggie');
+    expect(joinNames([{name: 'nick'}, {name: 'john'}, {name: 'ian'}, {name: 'ste'}])).toBe('nick, john, ian & ste');
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}])).toBe('Bart & Lisa');
+    expect(joinNames([{name: 'Bart'}])).toBe('Bart');
+  });
 });
