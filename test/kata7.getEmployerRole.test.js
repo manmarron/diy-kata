a228ed5a6d6bb33403132000ebc2cb76c944b3ab
Nick <nickmarron@hotmail.co.uk>
2020-05-07 21:20:42 +0100

    kata 7 complete - employer roles

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,29 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+
+  const employees = [{
+
+    name: 'Satti',
+  
+    role: 'Developer'
+  
+  }, {
+  
+    name: 'Jenny',
+  
+    role: 'Sales Associate'
+  
+  }, {
+  
+    name: 'Javid',
+  
+    role: 'Human Recommended Reading Assistant'
+  
+  }];
+
+  test("returns the employee's role in the company", () => {
+    
+    expect(getEmployerRole('Satti', employees)).toBe('Developer');
+  });
 });
